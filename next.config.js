421c8dc7ad037fd60b7d884caf3030d802f9da22
loom4k <contact@loom4k.me>
2022-06-03 08:54:30 -0400

    Update next.config.js for redirects

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const { withPlugins } = require('next-compose-plugins')
+const withImages = require('next-images')
+const withYAML = require('next-yaml')
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+module.exports = withPlugins([withImages, withYAML], {
+  reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/github',
+        destination: 'https://github.com/loom4k',
+        permanent: true,
+      }
+    ]
+  }
+})
