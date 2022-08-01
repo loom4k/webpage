2acf819b60d2c7c4fd08d62cc2bfb3cafd470dbe
loom4k <contact@loom4k.me>
2022-08-01 09:49:03 -0230

    Add redirect to Patreon page

--- next.config.js
+++ next.config.js
@@ -2,6 +2,15 @@
 const nextConfig = {
 	reactStrictMode: true,
 	output: "standalone",
+	async redirects() {
+		return [
+			{
+				source: "/go/patreon",
+				destination: "https://www.patreon.com/loom4k",
+				permanent: true
+			}
+		]
+	}
 };
 
 module.exports = nextConfig;
